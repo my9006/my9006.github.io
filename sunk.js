e7273bcd461f3c193b2993026195d85366fd9547
Mkhitar Mkrtchyan <mkhitarmkrtchyan@workfront.com>
2020-05-28 23:21:15 +0400

    Patch

--- sunk.js
+++ sunk.js
@@ -5,9 +5,9 @@ document.addEventListener('scroll', function(e) {
 		let currScrollPos2 = document.body.scrollTop;
 		headerComponent.style.opacity = currScrollPos2/100 - 2;
 	}
-	if(headerComponent.style.opacity>3.25){
+	if(headerComponent.style.opacity>3){
 		author.style.position ="fixed";
-		author.style.marginTop = "-526px";
+		author.style.marginTop = "-500px";
 	} else{
 		author.style.position="relative"
 	}
